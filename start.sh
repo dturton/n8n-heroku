94d1d53151b5784bee2b56ed0ea9ec1199546ebe
David Turton <dturton@etrivo.com>
2022-03-30 13:21:12 -0400

    update to worker

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 # check if port variable is set or go with default
 if [ -z ${PORT+x} ]; then echo "PORT variable not defined, leaving N8N to default port."; else export N8N_PORT=$PORT; echo "N8N will start on '$PORT'"; fi
 
-n8n
+n8n worker --concurrency=1
 
